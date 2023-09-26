a467330f8128a70ab946f5b1f87c907d8bb76f09
PetrLastovicka <123584550+PetrLastovicka@users.noreply.github.com>
2023-09-26 11:08:54 +0200

    initial commit

---
+++ src/main/java/com/pl/flightsmaven/FlightsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.pl.flightsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsMavenApplication.class, args);
+	}
+
+}
