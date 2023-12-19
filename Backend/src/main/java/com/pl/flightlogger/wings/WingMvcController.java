27bf6e319bc4af7ec144b7fe9cfe3d19d33a3737
PetrLastovicka <petr.lastovicka@gmail.com>
2023-12-19 21:37:23 +0100

    update wingInput

--- Backend/src/main/java/com/pl/flightlogger/wings/WingMvcController.java
+++ Backend/src/main/java/com/pl/flightlogger/wings/WingMvcController.java
@@ -2,10 +2,7 @@ package com.pl.flightlogger.wings;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,13 +22,23 @@ public class WingMvcController {
         model.addAttribute("wing", wingService.getById(id));
         return "wingDetails";
     }
+
     @GetMapping("/new")
     public String newWingForm(Model model){
+        model.addAttribute("title", "Add your new wing");
         return "wingInput";
     }
     @PostMapping("/new")
-    public String newWing(Model model){
-        
+    public String newWing(Model model, @RequestBody NewWingDTO request){
+        Wing wing = wingService.create(request);
+        model.addAttribute("wing", wing);
         return "wingDetails";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editWingForm(Model model,@PathVariable Long id){
+        model.addAttribute("title", "Edit your wing");
+        model.addAttribute("wing", wingService.getById(id));
+        return "wingInput";
     }
 }
