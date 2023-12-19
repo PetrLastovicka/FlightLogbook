b70d9b0bc4147be5ffe991c35317966ba1921256
PetrLastovicka <petr.lastovicka@gmail.com>
2023-12-19 19:07:49 +0100

    add wingInput

--- Backend/src/main/java/com/pl/flightlogger/wings/WingMvcController.java
+++ Backend/src/main/java/com/pl/flightlogger/wings/WingMvcController.java
@@ -1,12 +1,13 @@
 package com.pl.flightlogger.wings;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/mvc/wings")
@@ -26,7 +27,7 @@ public class WingMvcController {
     }
     @GetMapping("/new")
     public String newWingForm(Model model){
-        return "newWing";
+        return "wingInput";
     }
     @PostMapping("/new")
     public String newWing(Model model){
